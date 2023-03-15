02b2bb2da586663a568a3a17904aedcc6f17a3c3
projetogppag <100373889+projetosgppag@users.noreply.github.com>
2023-03-15 15:21:07 -0300

    2.0
    agora ele retorna com um "aprovado" ou "reprovado"

--- ExercicioAlunosDesafio/Aluno.cs
+++ ExercicioAlunosDesafio/Aluno.cs
@@ -2,12 +2,27 @@ namespace ExercicioAlunos;
 class Aluno
 {
     public string? nome;
-    public double nota1;
-    public double nota2;
 
-    public double obterMedia(){
+    public string pegaNome(){
+        System.Console.WriteLine("Digite o nome do aluno abaixo: ");
+        nome = Console.ReadLine();
+        return nome;
+    }
 
-        double media = (nota1+nota2)/2;
+    public int pegaContador(){
+        int contador;
+        System.Console.WriteLine("Digite a quantia de notas: ");
+        contador = Convert.ToInt32(Console.ReadLine());
+        return contador;
+    }
+    public double calculaMedia(int contador){
+        double somaN=0, holder;
+        for(int x = 0; x<contador; x++){
+            System.Console.WriteLine("Digite a nota: ");
+            holder = Convert.ToDouble(Console.ReadLine());
+            somaN+=holder;
+        }
+        double media = somaN/contador;
         return media;
     }
     public string estadoAluno(double media){ //obterSituacao
@@ -21,45 +36,7 @@ class Aluno
         
         return situacao;
     }
-    public void mensagem(){
-        double mediaCalculada = obterMedia();
-        string resultadoSituacao = estadoAluno(mediaCalculada);
-        System.Console.WriteLine(nome+" está " + resultadoSituacao + " com média de: "+mediaCalculada);
+    public void mostraDados(double media){
+        System.Console.WriteLine("O nome do aluno é: " + nome + " e a média das notas foi: " + media + ", portanto o " + nome + " foi: " + estadoAluno(media));
     }
-
-    // desafio aqui, 3 metodos aonde os 3 serao: pegaNome, fazTudoInator3.0 e mostraDados.
-
-    public string pegaNome(){
-        System.Console.WriteLine("Digite o nome do aluno abaixo: ");
-        string? nome2 = Console.ReadLine();
-        return nome2;
-    }
-
-    public double pegaQuantiaNotasERetornaMedia(){
-        int contador;
-        double somaN=0, holder;
-        System.Console.WriteLine("Digite a quantia de notas: ");
-        contador = Convert.ToInt32(Console.ReadLine());
-        for(int x = 0; x<contador; x++){
-            System.Console.WriteLine("Digite a nota: ");
-            holder = Convert.ToDouble(Console.ReadLine());
-            somaN+=holder;
-        }
-        double media = somaN/contador;
-        return media;
-    }
-    public void mostraDados(){
-        System.Console.WriteLine("O nome do aluno é: " + nome + " a média das notas foi: " + pegaQuantiaNotasERetornaMedia());
-    }
-
-    //construtor ebaaaa
-    
-    /*
-    public Aluno(string nome, double nota1, double nota2){
-
-        this.nome = nome;
-        this.nota1 = nota1;
-        this.nota2 = nota2;
-    }
-    */
 }
--- ExercicioAlunosDesafio/Program.cs
+++ ExercicioAlunosDesafio/Program.cs
@@ -3,12 +3,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        /*
-        Aluno a1 = new Aluno("Gabriel", 10, 10);
-        a1.mensagem();
-        */
         Aluno a1 = new Aluno();
         a1.pegaNome();
-        a1.mostraDados();
+        a1.mostraDados(a1.calculaMedia(a1.pegaContador()));
     }
 }
